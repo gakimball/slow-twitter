7937f69631b4833968fe812d5afa9335f161d03d
Geoff Kimball <geoff@geoffkimball.com>
2022-07-10 22:47:46 -0700

    Strip URLs for attached media from the text of a tweet

--- src/create-html.ts
+++ src/create-html.ts
@@ -7,24 +7,39 @@ import { Status } from 'twitter-api-types'
 Handlebars.registerHelper('fullText', (tweet: Status) => {
   const text = tweet.full_text ?? ''
   const urls = tweet.entities.urls ?? []
+  const media = tweet.entities.media ?? []
 
-  return alter(text, urls.map(url => {
-    const [start, end] = url.indices ?? [0, 0]
+  return alter(text, [
+    ...urls.map(url => {
+      const [start, end] = url.indices ?? [0, 0]
 
-    if (url.expanded_url === tweet.quoted_status_permalink?.expanded) {
+      // Remove the URL for the quoted status
+      if (url.expanded_url === tweet.quoted_status_permalink?.expanded) {
+        return {
+          start: Math.max(0, start - 1),
+          end,
+          str: '',
+        }
+      }
+
+      // Linkify external URLs
       return {
-        start: start - 1,
+        start: start,
+        end,
+        str: `<a href="${url.expanded_url}">${url.expanded_url}</a>`,
+      }
+    }),
+    // Remove the URLs for attached media
+    ...media.map(item => {
+      const [start, end] = item.indices ?? [0, 0]
+
+      return {
+        start: Math.max(0, start - 1),
         end,
         str: '',
       }
-    }
-
-    return {
-      start: start,
-      end,
-      str: `<a href="${url.expanded_url}">${url.expanded_url}</a>`,
-    }
-  }))
+    })
+  ])
 })
 
 const templatePath = path.join(process.cwd(), './template/tweet.hbs')
